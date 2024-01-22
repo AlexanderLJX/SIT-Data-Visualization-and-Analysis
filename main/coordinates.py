20925b8fc4138b721dd0080b1b9e46dfb69e5a25
suriya1201 <2001375A@student.tp.edu.sg>
2024-01-22 22:35:31 +0800

    got coordinates
    some of the coordinates are not accurate as the system is not able to find the coordinates

--- main/coordinates.py
+++ main/coordinates.py
@@ -3,24 +3,55 @@
 
 from geopy.geocoders import Nominatim      
 import pandas as pd
-import json
+import re
+
 
 ############### reading data  from csv file##################### 
-data = pd.read_csv(r'scraped_data_food.csv') #!!!!!!!!!!! remember to change the file location if you are runnign this code if needed!!!!!!!!!!!!!!!!!!!!!!!!!!
+data = pd.read_csv(r'scraped_data_food.csv') #!!!!!!!!!!! remember to change the file location if needed!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-string = data['Metadata'].to_string()
-metadata_list=json.loads(string)
-address_list=[]
-
+address_list= data['Address'].values.tolist()
+postal_list=[]
 
 
 
 
 
 
+for i in address_list:
+        postal_code_pattern = re.compile(r'\b\d{6}\b')
+        postal_codes=postal_code_pattern.findall(i)
+        postal_list.append(postal_codes)
+    
 
 
-geolocator = Nominatim(user_agent = 'coordinates')
-location = geolocator.geocode()
-print((location.latitude, location.longitude))
+longitude_list=[]
+latitude_list=[]
+for x in postal_list:
+    try:
+        geolocator = Nominatim(user_agent = 'coordinates')
+        location_query = f"{x}, Singapore"
+        location = geolocator.geocode(location_query)
+        longitude=location.longitude
+        latitude=location.latitude
+        longitude_list.append(longitude)
+        latitude_list.append(latitude)
+        
+    except Exception as e:
+        latitude= 1.287953
+        longitude= 103.851784
+        longitude_list.append(longitude)
+        latitude_list.append(latitude)
+
+
+
+
+
+data['latitude']=latitude_list
+data['longitude']= longitude_list
+
+data.to_csv('modified_data.csv')
+print("done")
+
+
+
 
