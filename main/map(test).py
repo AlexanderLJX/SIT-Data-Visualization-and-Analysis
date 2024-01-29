70e4a3582e8da4a0c7f2d9f8e06406f91bec64a4
suriya1201 <2001375A@student.tp.edu.sg>
2024-01-29 21:05:06 +0800

    simple plotting

--- main/map(test).py
+++ main/map(test).py
@@ -2,6 +2,33 @@ import folium
 import pandas as pd 
 
 import folium
+from branca.element import Figure
 
-m = folium.Map()
+
+
+df_coordinates=pd.read_csv('main/modified_data.csv')
+df_coordinates.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+fig=Figure(width=550,height=350)
+
+
+m=folium.Map(location=[1.287953, 103.851784],zoom_start=12)
+
+
+fig.add_child(m)
+
+folium.Marker(location=[1.33503545, 103.85177265],popup='Default popup Marker1',tooltip='Click here to see Popup').add_to(m)
+
 m.save("main/map.html")
