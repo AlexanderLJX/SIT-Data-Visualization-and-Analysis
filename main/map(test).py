f7a992db1a96c91d0dc355bfa6388ae6e3b3e047
suriya1201 <2001375A@student.tp.edu.sg>
2024-01-29 21:47:09 +0800

    plotted partial

--- main/map(test).py
+++ main/map(test).py
@@ -6,29 +6,22 @@ from branca.element import Figure
 
 
 
-df_coordinates=pd.read_csv('main/modified_data.csv')
-df_coordinates.head()
-
-
-
-
-
-
-
-
-
-
-
-
+df_data=pd.read_csv('main/modified_data.csv')
 
 fig=Figure(width=550,height=350)
-
-
 m=folium.Map(location=[1.287953, 103.851784],zoom_start=12)
-
-
 fig.add_child(m)
+coordinates=[]
+for i,n in enumerate(df_data['Name']):
+    a= [df_data['Name'][i],df_data['latitude'][i], df_data['longitude'][i]]
+    coordinates.append(a)
 
-folium.Marker(location=[1.33503545, 103.85177265],popup='Default popup Marker1',tooltip='Click here to see Popup').add_to(m)
+
+
+for c in coordinates:
+    rest=c[0]
+    lon=c[1]
+    lat=c[2]
+    folium.Marker(location=[lon, lat],popup=str(rest),tooltip='Click here to see restaurant').add_to(m)
 
 m.save("main/map.html")
