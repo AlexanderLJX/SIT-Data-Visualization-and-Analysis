be06ed17461ac31dda1f64665ff21e32439607ae
suriya1201 <2001375A@student.tp.edu.sg>
2024-01-30 23:14:49 +0800

    file export

--- main/gui.py
+++ main/gui.py
@@ -1,16 +1,17 @@
 import PySimpleGUI as sg
 import webbrowser
-
+import pandas as pd
 
 sg.theme('DarkAmber')  # Add a touch of color
 
-
+# Read data from the file
+df = pd.read_csv('main/scraped_data_food.csv')
 
 layout = [
     [sg.Text('What would you like to do today?')],
     [sg.Text('Choose one of the following options : ')],
     [],
-    [sg.Button('View all food places', key='-VIEW-ALL-', size=(30, 2), pad=(10,10)), sg.Button('View Dataset diagrams', key='-VIEW-DIAGRAMS-', pad=(10,10), size=(30, 2))],
+    [sg.Button('View all food places', key='-VIEW-ALL-', size=(30, 2), pad=(10,10)), sg.Button('View Dataset diagrams', key='-VIEW-DIAGRAMS-', pad=(10,10), size=(30, 2)), sg.Button('Export the Dataset', key='-EXPORT-', pad=(10,10), size=(30, 2))],
     [],
     [sg.Text('Choose the area in Singapore : '),sg.Combo(['Bukit Batok','Bukit Timah','Sengkang','Hougang'], key='-OPTION-', pad=(10,10), size=(30, 2))],
     [],
@@ -31,6 +32,10 @@ while True:
         webbrowser.open("main/map.html")
     elif event == '-VIEW-DIAGRAMS-':
         print('View Dataset diagrams')
+    elif event == '-EXPORT-':
+        filename = sg.popup_get_file('Select a file to save to', save_as=True, file_types=(("CSV Files", "*.csv"),))
+        if filename:
+            df.to_csv(filename, index=False)
     elif event == '-VIEW-AREAS-':
         print('View in areas of Singapore')
 
