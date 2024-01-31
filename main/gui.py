3d913e9974155a64d4144616214b651b0af57e2c
suriya1201 <2001375A@student.tp.edu.sg>
2024-01-31 09:04:42 +0800

    integrating

--- main/gui.py
+++ main/gui.py
@@ -1,6 +1,35 @@
 import PySimpleGUI as sg
 import webbrowser
 import pandas as pd
+import folium
+from branca.element import Figure
+
+
+
+df_data=pd.read_csv('main/modified_data.csv')
+
+fig=Figure(width=550,height=350)
+m=folium.Map(location=[1.287953, 103.851784],zoom_start=12)
+fig.add_child(m)
+coordinates=[]
+for i,n in enumerate(df_data['Name']):
+    a= [df_data['Name'][i],df_data['latitude'][i], df_data['longitude'][i]]
+    coordinates.append(a)
+
+
+
+for c in coordinates:
+    rest=c[0]
+    lon=c[1]
+    lat=c[2]
+    folium.Marker(location=[lon, lat],popup=str(rest),tooltip='Click here to see restaurant').add_to(m)
+
+m.save("main/map.html")
+
+
+
+
+
 
 sg.theme('DarkAmber')  # Add a touch of color
 
@@ -27,9 +56,9 @@ while True:
 
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
-
+        
     if event == '-VIEW-ALL-':
-        webbrowser.open("main/map.html")
+        webbrowser.open(r"main\map.html")
     elif event == '-VIEW-DIAGRAMS-':
         print('View Dataset diagrams')
     elif event == '-EXPORT-':
