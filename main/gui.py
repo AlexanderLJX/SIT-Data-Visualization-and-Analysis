c283347bb4d518ca08e7815e68ebec4f0f4a921d
suriya1201 <2001375A@student.tp.edu.sg>
2024-02-01 20:22:56 +0800

    able to filter the data

--- main/gui.py
+++ main/gui.py
@@ -21,11 +21,16 @@ except Exception as e:
     print(f"An error occurred while reading the CSV files: {e}")
     exit(1)
 
-def plotmap(value):
-    if value!='':
-       df_data_filtered = df_data.loc[df_data['Sub Area'] == value]
+def plotmap(value1, value2):
+    if value1!='':
+       df_data_filtered = df_data.loc[df_data['Sub Area'] == value1]
     else: 
         df_data_filtered=df_data
+
+    if value2 != '':
+        df_data_filtered = df_data_filtered.loc[df_data_filtered['Category'] == value2]
+    else :
+        df_data_filtered=df_data_filtered
     m=folium.Map(location=[1.287953, 103.851784],zoom_start=12,prefer_canvas=True)
     coordinates = df_data_filtered.apply(lambda row: [row['Name'], row['latitude'], row['longitude']], axis=1)
     marker_cluster = MarkerCluster().add_to(m)
@@ -42,22 +47,33 @@ def plotmap(value):
     return tmp.name
 
 
+
+#find the catgory types in the csv
+
+unique_cat = df_data['Category'].unique()
+unique_cat_list = list(map(str, unique_cat))
+
+
+# find the sub area types in the csv
+unique_Area = df_data['Sub Area'].unique()
+unique_Area_list = list(map(str, unique_Area))
+
 #creatig the gui
-font = ("Arial", 11)
+font = ("Arial",11)
 layout = [
     
     [sg.Text('What would you like to do today?', font=font)],
     [sg.Text('Choose one of the following options : ', font=font)],
     [],
-    [sg.Button('View all food places', key='-VIEW-ALL-', size=(30, 2), pad=(10,10)), sg.Button('View Dataset diagrams', key='-VIEW-DIAGRAMS-', pad=(10,10), size=(30, 2)), sg.Button('Export the Dataset', key='-EXPORT-', pad=(10,10), size=(30, 2) , font=font)],
+    [sg.Button('View all food places', key='-VIEW-ALL-', size=(30, 2), pad=(10,10)), sg.Button('View Dataset diagrams', key='-VIEW-DIAGRAMS-', pad=(10,10), size=(30, 2)), sg.Button('Export Dataset', key='-EXPORT-', pad=(10,10), size=(30,2), font=font)],
     [],
-    [sg.Text('Choose the area in Singapore : ', font=font),sg.Combo(['Bukit Merah','HarbourFront/Maritime Square','Depot Road','Hougang'], key='-OPTION-', pad=(10,10), size=(30, 2), font=font)],
+    [sg.Text('Choose the area in Singapore : ', font=font),sg.Combo(values=unique_Area_list, key='-OPTION-', pad=(10,10), size=(30, 20), font=font),sg.Text('Choose the Category of Foodplace : ', font=font),sg.Combo(values=unique_cat_list, key='-OPTION2-', pad=(10,10), size=(30, 20), font=font)],
     [sg.Text( font=font)],
     [sg.Button('Ok', size=(3, 2), font=font), sg.Button('Cancel', size=(6, 2), font=font)],
     [],
 ]
 
-window = sg.Window('Foodplaces in Singapore', layout, size=(1000, 300),element_justification='center', resizable=True, finalize=True)
+window = sg.Window('Foodplaces in Singapore', layout, size=(1000,300),element_justification='center', resizable=True, finalize=True)
 temp_file_name= None
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -69,7 +85,7 @@ while True:
         break
         
     if event == '-VIEW-ALL-':
-        temp_file_name=plotmap('')
+        temp_file_name=plotmap('','')
         
     elif event == '-VIEW-DIAGRAMS-':
         print('View Dataset diagrams')
@@ -82,8 +98,9 @@ while True:
 
 
     elif event == 'Ok' :
-         value=values['-OPTION-']
-         temp_file_name =plotmap(value)
+        value1 = values['-OPTION-']
+        value2 = values['-OPTION2-']
+        temp_file_name = plotmap(value1, value2)
     
 
 
