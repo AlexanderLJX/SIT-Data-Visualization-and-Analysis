73546a35125949045161a6d3ce06ef03263c3e37
suriya1201 <2001375A@student.tp.edu.sg>
2024-02-02 20:15:38 +0800

    Update gui.py

--- main/gui.py
+++ main/gui.py
@@ -13,27 +13,33 @@ sg.theme('DarkAmber')  # Add a touch of color
 try:
     df = pd.read_csv('main/scraped_data_food.csv')
     df_data=pd.read_csv('main/modified_data.csv')
+#exception if the file cant be found 
 except FileNotFoundError:
     print("One or both CSV files could not be found.")
     exit(1)
+#exception if there are issues reading the csv file
 except Exception as e:
     print(f"An error occurred while reading the CSV files: {e}")
     exit(1)
 
+
+#defining a function to plot the locations on the map
 def plotmap(value1, value2):
+    #if value 1 is not empty if filters out the data else it will take the whole dataset
     if value1!='':
        df_data_filtered = df_data.loc[df_data['Sub Area'] == value1]
     else: 
         df_data_filtered=df_data
-
+   #if value 2 is not empty it filters out the data either from value or it does not 
     if value2 != '':
         df_data_filtered = df_data_filtered.loc[df_data_filtered['Category'] == value2]
     else :
         df_data_filtered=df_data_filtered
+    #setting the map and the geo location that we want the users to focus on (e.g. Singapore) by using SG coordininates
     m=folium.Map(location=[1.287953, 103.851784],zoom_start=12,prefer_canvas=True)
+    #getting the coordinates from the data set then adding the markers
     coordinates = df_data_filtered.apply(lambda row: [row['Name'], row['latitude'], row['longitude']], axis=1)
     marker_cluster = MarkerCluster().add_to(m)
-    
     for coord in coordinates:
         folium.Marker(location=[coord[1], coord[2]], popup=str(coord[0]), tooltip='Click here to see restaurant').add_to(marker_cluster)
 
@@ -41,24 +47,22 @@ def plotmap(value1, value2):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".html") as tmp:
         tmp.write(m._repr_html_().encode('utf-8'))
         tmp.close()
-    # Open the temporary file in a web browser
+    # Open the temporary file in a web browser (better for rendering)
     webbrowser.open("file://" + os.path.realpath(tmp.name))
-    # Return the name of the temporary file
+    # Return the name of the temporary file so that we can delete the temp file after the user closes the window
     return tmp.name
 
 
 
-#find the catgory types in the csv
-
+#find the catgory types in the csv to use the values to create a dropdown list in the GUI
 unique_cat = df_data['Category'].unique()
 unique_cat_list = list(map(str, unique_cat))
 
-
-# find the sub area types in the csv
+# find the sub area types in the csv to use the values to create a dropdown list in the GUI
 unique_Area = df_data['Sub Area'].unique()
 unique_Area_list = list(map(str, unique_Area))
 
-#creatig the gui
+#creating the gui / formatting the layout of the GUI
 font = ("Arial",11)
 layout = [
     
@@ -78,6 +82,7 @@ layout = [
 
 window = sg.Window('Foodplaces in Singapore', layout, size=(1000,350),element_justification='center', resizable=True, finalize=True)
 temp_file_name= None
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
@@ -88,30 +93,36 @@ while True:
         break
         
     if event == '-VIEW-ALL-':
+        #viewing the map without any filters
         temp_file_name=plotmap('','')
         
     elif event == '-VIEW-DIAGRAMS-':
+        # adding the viewing of the dataset diagrams that others supposed to create 
         print('View Dataset diagrams')
 
 
     elif event == '-EXPORT-':
+        #exporting the full dataset
         filename = sg.popup_get_file('Select a file to save to', save_as=True, file_types=(("CSV Files", "*.csv"),))
         if filename:
             df.to_csv(filename, index=False)
 
 
     elif event == 'Ok' :
+        #view the map with the Category of restaurant or the sub area that it is in
         value1 = values['-OPTION-']
         value2 = values['-OPTION2-']
         temp_file_name = plotmap(value1, value2)
     
     elif event == '-EXPORT-MAP-':
+        #exporting of the map 
         if temp_file_name is not None:
             destination = sg.popup_get_file('Select a file to save the map HTML', save_as=True, file_types=(("HTML Files", "*.html"),))
             if destination:
                 shutil.copy(temp_file_name, destination)
 
     elif event == '-EXPORT-FILTERED-':
+        #exporting of the filtered dataset
         value1 = values['-OPTION-']
         value2 = values['-OPTION2-']
         if value1!='':
